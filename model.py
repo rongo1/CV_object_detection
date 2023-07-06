5bdf16a5972ae6467f5814623dc7fbbefd48683a
Yusif-bit <75015940+Yusif-bit@users.noreply.github.com>
2023-07-06 12:52:15 +0400

    yolov1_final

--- model.py
+++ model.py
@@ -26,7 +26,7 @@ architecture_config = [
 class CNNBlock(nn.Module):
     def __init__(self, in_channels, out_channels, **kwargs):
         super(CNNBlock, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, bias = False, kernel_size = 1, **kwargs)
+        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
         self.batchnorm = nn.BatchNorm2d(out_channels)
         self.leakyrelu = nn.LeakyReLU(0.1)
 
@@ -37,43 +37,45 @@ class Yolov1(nn.Module):
     def __init__(self, in_channels = 3, **kwargs):
         super(Yolov1, self).__init__()
         self.architecture = architecture_config
+        self.in_channels = in_channels
         self.darknet = self._create_conv_layers(self.architecture)
         self.fcs = self._create_fcs(**kwargs)
     
     def forward(self, x):
         x = self.darknet(x)
-        return self.fcs(torch.flatter(x, start_dim = 1))
+        print(x.shape)
+        return self.fcs(torch.flatten(x, start_dim = 1))
     def _create_conv_layers(self, architecture):
         layers = []
         in_channels = self.in_channels
-        for x in self.architecture:
+        for x in architecture:
             if type(x) == tuple:
-                layers += [self.CNNBlock(in_channels, x[1], kernel_size = x[0], stride = x[2], padding = x[3])]
-                in_channels = x[1]
+                layers += [CNNBlock(in_channels, x[1], kernel_size=x[0], stride=x[2], padding=x[3],)] 
+                in_channels = x[1] 
             elif type(x) == str:
                 layers += [nn.MaxPool2d(kernel_size = (2, 2), stride = (2, 2))]
-            else:
+            elif type(x) == list:
                 clayer1 = x[0]
                 clayer2 = x[1]
                 num_rep = x[2]
                 for _ in range(num_rep):              
-                    layers += [self.CNNBlock(in_channels, clayer1[1], kernel_size = clayer1[0], stride = clayer1[1], padding = clayer1[2])]
-                    layers += [self.CNNBlock(clayer1[1], clayer2[1], kernel_size = clayer2[0], stride = clayer2[2], padding = clayer2[3])]
-                    in_channels = clayer2[2]
+                    layers += [CNNBlock(in_channels, clayer1[1], kernel_size = clayer1[0], stride = clayer1[2], padding = clayer1[3],)]
+                    layers += [CNNBlock(clayer1[1], clayer2[1], kernel_size = clayer2[0], stride = clayer2[2], padding = clayer2[3],)]
+                    in_channels = clayer2[1]
         return nn.Sequential(*layers)
     def _create_fcs(self, split_size, num_boxes, num_classes):
         return nn.Sequential(
             nn.Flatten(),
             nn.Linear(1024*split_size*split_size, 496),
             nn.Dropout(0.0),
-            nn.LeakyRelu(0.1),
+            nn.LeakyReLU(0.1),
             nn.Linear(496, split_size*split_size* (num_classes  + num_boxes*5))
 
         )
 
-def test(split_size = 7, num_boxes = 2, num_classes = 20):
-    model = Yolov1(split_size, num_boxes, num_classes)
-    x = torch.rand((2, 488, 488, 3))
+def test(S = 7, B = 2, C = 20):
+    model = Yolov1(split_size = S, num_boxes = B, num_classes = C)
+    x = torch.randn((2, 3, 448, 448))
     print(model(x).shape)   
 test()
     
